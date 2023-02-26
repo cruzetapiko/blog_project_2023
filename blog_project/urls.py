01f09c084c3620968395c3a462251e8a3096ecac
cruzetapiko <simonetvia@outlook.com>
2023-02-25 20:38:19 -0700

    logout and profile links completed no errors

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from user_app import views as  user_views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     #path to django admin
     path('admin/', admin.site.urls),
@@ -8,5 +11,7 @@ urlpatterns = [
     #path to blog_app application
     path('', include('blog_app.urls')),
 
-    
+    #user_app paths
+    path("logout/", auth_views.LogoutView.as_view(template_name='user_app/logout.html'), name="user_app-logout"),
+    path("profile/", user_views.profile, name="user_app-profile"),
 ]
