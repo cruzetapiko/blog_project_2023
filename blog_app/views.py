5000590a3558482087ffc28ac79f0cf77f4eb677
cruzetapiko <simonetvia@outlook.com>
2023-02-25 20:06:42 -0700

    setting up layout and index template

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post 
 
-# Create your views here.
+def home(request):
+
+    context = {
+        'posts': Post.objects.all()
+    }
+
+    return render(request, 'blog_app/index.html', context)
+
+def about(request):
+
+    return render(request, 'blog_app/about.html')
