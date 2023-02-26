788c18a7534b4c33b4c88a4f78788c045df5515c
cruzetapiko <simonetvia@outlook.com>
2023-02-25 20:45:50 -0700

    user_app login, logout,profile completed no errors

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -115,6 +115,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
+LOGIN_REDIRECT_URL = 'blog_app-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
