966ec1ac81e2f6070dc0015991786d160d41b9fc
cruzetapiko <simonetvia@outlook.com>
2023-02-26 08:08:30 -0700

    register form no crispy

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', include('blog_app.urls')),
 
     #user_app paths
+    path("register/", user_views.register, name="user_app-register"),
     path("login/", auth_views.LoginView.as_view(template_name='user_app/login.html'), name="user_app-login"),
     path("logout/", auth_views.LogoutView.as_view(template_name='user_app/logout.html'), name="user_app-logout"),
     path("profile/", user_views.profile, name="user_app-profile"),
--- user_app/views.py
+++ user_app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 @login_required
 def profile(request):
     return render(request, 'user_app/profile.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'your account has been created {username}!')
+            return redirect('user_app-login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user_app/register.html', {'form': form})
