e1d4fe6127cd513444d791c1d32f6476d744ee85
cruzetapiko <simonetvia@outlook.com>
2023-02-28 20:00:02 -0700

    listview and detailview complete

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Post 
 
+from django.views.generic import ListView, DetailView
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog_app/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'  
+    ordering = ['-date_posted'] #organice the posts by date published
+
+class PostDetailView(DetailView):
+    model = Post
+   
+
 def home(request):
 
     context = {
