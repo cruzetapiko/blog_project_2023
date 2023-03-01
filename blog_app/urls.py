27b8e8e9292a17911ea35b38f2075b0494261957
cruzetapiko <simonetvia@outlook.com>
2023-03-01 14:55:28 -0700

    create and update views

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import *
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog_app-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='blog_app-post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='blog_app-post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='blog_app-post-update'),
     path('about/',views.about, name='blog_app-about'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 class PostListView(ListView):
     model = Post
@@ -11,7 +12,33 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog_app/new_post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog_app/new_post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
    
+
+
 
 def home(request):
 
