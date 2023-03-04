09a0d76342e25bccf9fef42de5daa7288d4804d1
cruzetapiko <simonetvia@outlook.com>
2023-03-03 21:28:43 -0700

    project completed

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -133,3 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+DJANGO_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'titicotamo@gmail.com'
+EMAIL_HOST_PASSWORD = 'nkcbeouqdqglhncv'
